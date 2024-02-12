2a5ee6566e091e4412cf7fbb392a7d331acaf058
ben3101 <111755107+ben3101@users.noreply.github.com>
2024-02-12 17:43:56 +0000

    moved vp/vc functionality to  separate User app

--- src/index.js
+++ src/index.js
@@ -4,85 +4,9 @@ import { ES256KSigner, hexToBytes } from 'did-jwt';
 const searchBar = document.querySelector('#searchBar');
 const getBtn = document.querySelector('#getBtn');
 const responseArea = document.querySelector('#responseArea');
-
-//----------------------------------------------------------------------------------------------------------
-
 let appName;
 let user;
 let issuer;
-
-//Example VC/VP code----------------------------
-async function createVP(nonce, domain, user, issuer, appName){
-  // Create a signer by using a private key (hex).
-  //ben3101 key
-  //const VcIssuerKey = 'a17cb543a7fbf5493a9754c977826925a346964c5b292e9da31bb6940f698313';
-  //issuer123 key
-  const VcIssuerKey = '2143c4bd995378ce36bacfcfda2e39610f2809e349b4d25e7b7d2b5f1d82e6ae';
-  const VcSigner = ES256KSigner(hexToBytes(VcIssuerKey))
-
-  // Prepare an issuer (of VC)
-  const vcIssuer = {
-      did: issuer,
-      signer: VcSigner
-  }
-
-  //Create a VC:
-  //use today's date for issuance
-  let today = Math.ceil((Date.now() / 1000));
-  let tenYearsFromNow = today + 315569260;
-
-  //test dates for expired VC, make issuance 10 years ago and expiry yesterday
-  // tenYearsFromNow = today - 86400;
-  // today = today - 315569260;
-  //-payload
-  const vcPayload = {
-    sub: user,
-    //nbf: 1562950282,
-    nbf: today,
-    exp: tenYearsFromNow,
-    vc: {
-      "@context": [
-        "https://www.w3.org/2018/credentials/v1",
-        "https://www.w3.org/2018/credentials/examples/v1"
-      ],
-      type: ["VerifiableCredential", "UniversityDegreeCredential"],
-      credentialSubject: {
-        degree: {
-          type: "BachelorDegree",
-          name: "Bachelor of Science"
-        }
-      }
-    }
-  }
-  //-convert VC to JWT
-  const vcJwt = await createVerifiableCredentialJwt(vcPayload, vcIssuer);
-
-  //Create a VP:
-  //the VC holder will sign the VP
-  //ben3101 key
-  const VpSignerKey = 'a17cb543a7fbf5493a9754c977826925a346964c5b292e9da31bb6940f698313';
-  //issuer123 key
-  //const VpSignerKey = '2143c4bd995378ce36bacfcfda2e39610f2809e349b4d25e7b7d2b5f1d82e6ae';
-  const VpSigner = ES256KSigner(hexToBytes(VpSignerKey))
-  const holder = {
-    did: user,
-    signer: VpSigner
-}
-
-  //-VP payload
-  const vpPayload = {
-    vp: {
-      '@context': ['https://www.w3.org/2018/credentials/v1'],
-      type: ['VerifiablePresentation'],
-      verifiableCredential: [vcJwt],
-      nonce: nonce,
-      domain: domain,
-      appName: appName
-    }
-  }
-  const vpJwt = await createVerifiablePresentationJwt(vpPayload, holder);
-  return vpJwt;
-}
 
 //Requests--------------------------------------------
 getBtn.onclick = function () {
@@ -95,6 +19,112 @@ getBtn.onclick = function () {
 
 //---------------------------------------------------------------------------------------
 async function VcBasedrequests(){
+  console.log("Sending first request for the resource...");
+  //message containing the app, issuer and user, requesting the resource at the url entered in the input box
+  let url = searchBar.value;
+  let response = await fetch(url, {
+    method: "POST",
+    headers: {
+      'vc': 'true'//so the server's VcHttpHandler component handles it
+    },
+    body: JSON.stringify({
+      'user': user,
+      'app': appName,
+      'vcissuer': issuer,
+    })
+  });
+  let result = await response.json();
+  console.log(result);
+  console.log(response);
+  responseArea.innerHTML = JSON.stringify(result);
+  let VPrequest = result;
+
+  let nonce = undefined;
+  let domain = undefined;
+  try{
+    nonce = VPrequest.VerifiablePresentation.challenge;
+    domain = VPrequest.VerifiablePresentation.domain;
+  }catch(e){
+    console.log("No nonce or domain received in response");
+    return;
+  }
+
+  let resource = await requestWithVP(nonce, domain, url);
+
+  //wait 5 secs before updating the display
+  console.log(resource);
+  window.setTimeout(()=>{
+    responseArea.innerHTML = resource;
+  }, 5000);
+}
+
+//sends request to User app and asks for a VP
+async function getVP(nonce, domain){
+  console.log("Sending request to the User app...");
+  let url = 'http://localhost:8081/vprequest';
+  let response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json"
+     },
+    body: JSON.stringify({
+      user: user,
+      application: appName,
+      vcissuer: issuer,
+      nonce: nonce,
+      domain: domain
+    })
+  });
+  let result = await response.text();
+  console.log(result);
+  console.log(response);
+  let VP = result;
+  return VP;
+}
+
+async function requestWithVP(nonce, domain, url){  
+  if(nonce !== undefined && domain !== undefined){
+    let vpJwt = await getVP(nonce, domain)
+    let msg = {
+      method: "GET",
+      headers: {
+        'vp': vpJwt,
+        }
+      }
+    let response2 = await fetch(url, msg);
+    console.log(`Sent second request: ${JSON.stringify(msg)}`);
+    let res = response2.text();
+    return res;
+    }else{
+      console.log("Could not send second request.")
+    }
+}
+
+async function oldrequestWithVP(nonce, domain, url){
+  //the response from the server should be a VP request
+  //if second response received with nonce, send them inside the VP
+  //create a VC, include it inside the VP
+  //send the VP inside the header of the GET request
+  
+  if(nonce !== undefined && domain !== undefined){
+    let vpJwt = await createVP(nonce, domain, user, issuer, appName);
+    let msg = {
+      method: "GET",
+      headers: {
+        'vp': vpJwt,
+        }
+      }
+    let response2 = await fetch(url, msg);
+    console.log(`Sent second request: ${JSON.stringify(msg)}`);
+    let res = response2.text();
+    return res;
+    }else{
+      console.log("Could not send second request.")
+    }
+}
+
+async function oldVcBasedrequests(){
   console.log("Sending first request for the resource...");
   //message containing the app, issuer and user, requesting the resource at the url entered in the input box
   let url = searchBar.value;
@@ -136,25 +166,75 @@ async function VcBasedrequests(){
   }, 5000);
 }
 
-async function requestWithVP(nonce, domain, url){
-  //the response from the server should be a VP request
-  //if second response received with nonce, send them inside the VP
-  //create a VC, include it inside the VP
-  //send the VP inside the header of the GET request
-  
-  if(nonce !== undefined && domain !== undefined){
-    let vpJwt = await createVP(nonce, domain, user, issuer, appName);
-    let msg = {
-      method: "GET",
-      headers: {
-        'vp': vpJwt,
-        }
-      }
-    let response2 = await fetch(url, msg);
-    console.log(`Sent second request: ${JSON.stringify(msg)}`);
-    let res = response2.text();
-    return res;
-    }else{
-      console.log("Could not send second request.")
-    }
-}
+//Example VC/VP code----------------------------
+// async function createVP(nonce, domain, user, issuer, appName){
+//   // Create a signer by using a private key (hex).
+//   //ben3101 key
+//   //const VcIssuerKey = 'a17cb543a7fbf5493a9754c977826925a346964c5b292e9da31bb6940f698313';
+//   //issuer123 key
+//   const VcIssuerKey = '2143c4bd995378ce36bacfcfda2e39610f2809e349b4d25e7b7d2b5f1d82e6ae';
+//   const VcSigner = ES256KSigner(hexToBytes(VcIssuerKey))
+
+//   // Prepare an issuer (of VC)
+//   const vcIssuer = {
+//       did: issuer,
+//       signer: VcSigner
+//   }
+
+//   //Create a VC:
+//   //use today's date for issuance
+//   let today = Math.ceil((Date.now() / 1000));
+//   let tenYearsFromNow = today + 315569260;
+
+//   //test dates for expired VC, make issuance 10 years ago and expiry yesterday
+//   // tenYearsFromNow = today - 86400;
+//   // today = today - 315569260;
+//   //-payload
+//   const vcPayload = {
+//     sub: user,
+//     //nbf: 1562950282,
+//     nbf: today,
+//     exp: tenYearsFromNow,
+//     vc: {
+//       "@context": [
+//         "https://www.w3.org/2018/credentials/v1",
+//         "https://www.w3.org/2018/credentials/examples/v1"
+//       ],
+//       type: ["VerifiableCredential", "UniversityDegreeCredential"],
+//       credentialSubject: {
+//         degree: {
+//           type: "BachelorDegree",
+//           name: "Bachelor of Science"
+//         }
+//       }
+//     }
+//   }
+//   //-convert VC to JWT
+//   const vcJwt = await createVerifiableCredentialJwt(vcPayload, vcIssuer);
+
+//   //Create a VP:
+//   //the VC holder will sign the VP
+//   //ben3101 key
+//   const VpSignerKey = 'a17cb543a7fbf5493a9754c977826925a346964c5b292e9da31bb6940f698313';
+//   //issuer123 key
+//   //const VpSignerKey = '2143c4bd995378ce36bacfcfda2e39610f2809e349b4d25e7b7d2b5f1d82e6ae';
+//   const VpSigner = ES256KSigner(hexToBytes(VpSignerKey))
+//   const holder = {
+//     did: user,
+//     signer: VpSigner
+// }
+
+//   //-VP payload
+//   const vpPayload = {
+//     vp: {
+//       '@context': ['https://www.w3.org/2018/credentials/v1'],
+//       type: ['VerifiablePresentation'],
+//       verifiableCredential: [vcJwt],
+//       nonce: nonce,
+//       domain: domain,
+//       appName: appName
+//     }
+//   }
+//   const vpJwt = await createVerifiablePresentationJwt(vpPayload, holder);
+//   return vpJwt;
+// }
